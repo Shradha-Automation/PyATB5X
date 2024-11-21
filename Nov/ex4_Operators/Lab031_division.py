db18513c50db11035fbfca8e3de3b233a451a4cf
shradha <shradhanjalli.priyadarshini07@gmail.com>
2024-11-21 15:06:44 +0530

    21st Nov' 2024

---
+++ Nov/ex4_Operators/Lab031_division.py
@@ -0,0 +1,10 @@
+print (5/3) # Division (float)
+print (type(5/3))
+print(5//3) # Quotient (int)
+print (type (5//3))
+
+print (10/2)
+print (10//2)
+
+print (146/3)
+print(146//3)
---
+++ Nov/ex4_Operators/Lab032_CompareOperator.py
@@ -0,0 +1,5 @@
+#compare operator
+print (2 ==2)
+print (2==3)
+print (5==5)
+print(5==7)
